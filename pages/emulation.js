9b416c4bc96382a19a932369760f6411e6f39459
ge39 <ge39.medeiros@gmail.com>
2025-01-01 10:09:25 -0300

    Update emulation.js

--- pages/emulation.js
+++ pages/emulation.js
@@ -21,14 +21,14 @@ export default function Emulation() {
         EJS_color: "#0064ff",
         EJS_gameUrl: `../roms/${jogo}`,
         EJS_biosUrl: "",
-        EJS_pathtodata:"https://cdn.emulatorjs.org/stable/data/",
+        // EJS_pathtodata:"https://cdn.emulatorjs.org/stable/data/",
       };
   
       Object.assign(window, emulatorConfig);
   
       // Carregar o script do EmulatorJS
       const script = document.createElement("script");
-      script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
+      script.src = "https://www.emulatorjs.com/loader.js";
       script.async = true;
   
       script.onload = () => {
