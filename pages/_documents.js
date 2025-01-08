0a2ce7c082ae274e1bfbbddb6f2c2afe9e74481f
ge39 <ge39.medeiros@gmail.com>
2025-01-08 13:59:37 -0300

    update fonts google e font retro

--- pages/_documents.js
+++ pages/_documents.js
@@ -1,22 +1,24 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
- 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Fonte Roboto */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* Fonte retrô (Press Start 2P) */}
+        <link
+          href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
- 
-export default MyDocument
