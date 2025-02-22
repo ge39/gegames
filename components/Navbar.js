fdc831bfbb23d1cb236100dab145eef5b70d39b6
ge39 <ge39.medeiros@gmail.com>
2025-02-22 00:35:57 -0300

    atualizado navbar e corrigido roms arcade

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,6 @@ import OnlineCounter from './OnlineCounter';
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-  const [submenuOpen, setSubmenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -15,7 +14,6 @@ export default function Navbar() {
 
   return ( 
     <>
-                  
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/">Gegames</Link>
@@ -28,44 +26,24 @@ export default function Navbar() {
         </div>
 
         <ul className={`${styles.menu} ${isOpen ? styles.menuOpen : ''}`}>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/jogos-online-retro ">Jogue Online</Link>
-          </li>
-          <li>
-            <Link href="/#orcamento">Fale Conosco</Link>
-          </li>
-          <li>
-            <Link href="/#nosso-plano">Nossos Planos</Link>
-          </li>
-          <li>
-            <Link href="/#eventos">Eventos</Link>
-          </li>
-
-          <li
-            className={styles.hasSubmenu}
-            onMouseEnter={() => setSubmenuOpen(true)}
-            onMouseLeave={() => setSubmenuOpen(false)}
-          >
+          <li><Link href="/">Home</Link></li>
+          <li><Link href="/jogos-online-retro">Jogue Online</Link></li>
+          <li><Link href="/#orcamento">Fale Conosco</Link></li>
+          <li><Link href="/#nosso-plano">Nossos Planos</Link></li>
+          <li><Link href="/#eventos">Eventos</Link></li>
+          
+          <li className={styles.hasSubmenu}>
             <Link href="#">Imagem</Link>
-            {submenuOpen && (
-              <ul className={styles.submenu}>
-                <li><Link href="/imageLess">Reduz Imagem</Link></li>
-                <li><Link href="/imagePlus">Amplia Imagem</Link></li>
-              </ul>
-            )}
+            <ul className={styles.submenu}>
+              <li><Link href="/imageLess">Reduz Imagem</Link></li>
+              <li><Link href="/imagePlus">Amplia Imagem</Link></li>
+            </ul>
           </li>
-          <li>
-            <Link href="/#sobre">Sobre</Link>
-          </li>
+          
+          <li><Link href="/#sobre">Sobre</Link></li>
         </ul>
       </nav>
-      <div>
-
-</div>
-< OnlineCounter />
+      <OnlineCounter />
     </>
   );
 }
