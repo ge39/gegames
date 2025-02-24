2054e027c5a6d8c54737b8f5ef8eb2bc085d0d5b
ge39 <ge39.medeiros@gmail.com>
2025-02-24 18:02:07 -0300

    inserindo as roms de arcade

--- pages/lista-de-jogos-snes-online.js
+++ pages/lista-de-jogos-snes-online.js
@@ -21,7 +21,7 @@ export default function Gamelist() {
       "releasedate": "1990-12-16"
     },
     {
-      "id": "aladdin",
+      "id": "Aladdin",
       "path": "snes/Aladdin (U) [T+Por].zip",
       "core": "snes",
       "name": "Aladdin",
