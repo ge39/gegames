813ea17132989cadd3a524e58cf3ee922547a9db
ge39 <ge39.medeiros@gmail.com>
2025-05-18 11:05:52 -0300

    Update arcadeEmulation.js

--- pages/arcadeEmulation.js
+++ pages/arcadeEmulation.js
@@ -18,7 +18,7 @@ export default function Emulation() {
       window.EJS_color = "#0000"; 
       window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/"; 
       // window.EJS_startOnLoaded = true;
-      window.EJS_gameUrl = `../../roms/${jogo}`; 
+      window.EJS_gameUrl = '../../roms/${jogo}'; 
       window.EJS_biosUrl = ""; 
 
       
