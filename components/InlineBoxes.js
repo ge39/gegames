094c3323181c45c22e315f8c1d3c0b5cea3d27c7
ge39 <ge39.medeiros@gmail.com>
2025-06-12 12:27:14 -0300

    Update InlineBoxes.js

--- components/InlineBoxes.js
+++ components/InlineBoxes.js
@@ -3,7 +3,25 @@ import styles from './InlineBoxes.module.css';
 export default function InlineBoxes() {
   return (
     <div>
-      <div className={styles.box}>Box 1</div>
+      <div className={styles.box}>
+        <button
+              onClick={connectToPeer}
+              disabled={!remoteId}
+              style={{
+                width: "100%",
+                padding: "10px 0",
+                background: !remoteId ? "#555" : "#2ea44f",
+                color: "#fff",
+                border: "none",
+                borderRadius: 6,
+                cursor: !remoteId ? "not-allowed" : "pointer",
+                fontSize: 14,
+                fontWeight: "700",
+              }}
+            >
+              Conectar
+            </button> 
+    </div>
       <div className={styles.box}>Box 2</div>
       <div className={styles.box}>Box 3</div>
       <div className={styles.box}>Box 4</div>
