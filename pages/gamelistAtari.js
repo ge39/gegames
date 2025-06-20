2dd88953b1aebb747c3a9b2451db0b7751177825
ge39 <ge39.medeiros@gmail.com>
2025-06-19 22:38:25 -0300

    Update gamelistAtari.js

--- pages/gamelistAtari.js
+++ pages/gamelistAtari.js
@@ -1,5 +1,4 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import styles from '../styles/GamelistArcade.module.css';
