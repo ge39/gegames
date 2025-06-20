c8ffd3df75b428ffa501e9549992e7f3b7b8a54e
ge39 <ge39.medeiros@gmail.com>
2025-06-19 22:04:54 -0300

    Update psxGames.js

--- data/psxGames.js
+++ data/psxGames.js
@@ -1,1 +1,124 @@
 
+
+export const psxGames = [
+  {
+    id: "1",
+    name: "Final Fantasy VII",
+    path: "psx/Final Fantasy VII (USA).zip",
+    core: "beetle-psx",
+    bios: "SCPH1001.BIN",
+    desc: "Um épico RPG com narrativa envolvente e personagens memoráveis. Revolucionou os RPGs nos consoles.",
+    image: "/images/games/psx/final-fantasy-vii-thumb.jpg",
+    rating: "0.98",
+    players: "1",
+    releasedate: "1997-06-01"
+  },
+  {
+    id: "2",
+    name: "Metal Gear Solid",
+    path: "psx/Metal Gear Solid (USA).zip",
+    core: "beetle-psx",
+    bios: {
+      necessario: true,
+      arquivo: "SCPH1001.BIN"
+    },
+    desc: "Ação furtiva cinematográfica com enredo complexo e jogabilidade estratégica. Um marco da série.",
+    image: "/images/games/psx/metal-gear-solid-thumb.jpg",
+    rating: "0.97",
+    players: "1",
+    releasedate: "1998-06-01"
+  },
+  {
+    id: "4",
+    name: "Resident Evil 2",
+    path: "psx/Resident Evil 2 (USA).zip",
+    core: "beetle-psx",
+    bios: {
+      necessario: true,
+      arquivo: "SCPH1001.BIN"
+    },
+    desc: "Terror de sobrevivência com narrativa ramificada e atmosfera assustadora.",
+    image: "/images/games/psx/resident-evil-2-thumb.jpg",
+    rating: "0.94",
+    players: "1",
+    releasedate: "1998-06-01"
+  },
+  {
+    id: "5",
+    name: "Gran Turismo 2",
+    path: "psx/Gran Turismo 2 (USA).zip",
+    core: "beetle-psx",
+    bios: {
+      necessario: true,
+      arquivo: "SCPH1001.BIN"
+    },
+    desc: "Simulação de corrida com centenas de carros e pistas. Um dos mais completos do PS1.",
+    image: "/images/games/psx/gran-turismo-2-thumb.jpg",
+    rating: "0.93",
+    players: "1-2",
+    releasedate: "1999-06-01"
+  },
+  {
+    id: "6",
+    name: "Crash Bandicoot: Warped",
+    path: "psx/Crash Bandicoot 3 Warped.PBP",
+    core: "psx",
+    bios: "/roms/psx/bios/SCPH1001.BIN",
+    desc: "Aventura colorida com fases variadas e muito humor. Um clássico da Naughty Dog.",
+    image: "/roms/psx/images/Crash Bandicoot 3 Warped-thumb.png",
+    rating: "0.92",
+    players: "1",
+    releasedate: "1998-06-01"
+  },
+  {
+    id: "7",
+    name: "Tekken 3",
+    path: "psx/Tekken 3 (USA).zip",
+    core: "beetle-psx",
+    bios: {
+      necessario: true,
+      arquivo: "SCPH1001.BIN"
+    },
+    desc: "Luta frenética com personagens icônicos e jogabilidade refinada.",
+    image: "/images/games/psx/tekken-3-thumb.jpg",
+    rating: "0.91",
+    players: "1-2",
+    releasedate: "1998-06-01"
+  },
+  {
+    id: "8",
+    name: "Tony Hawk’s Pro Skater 2",
+    path: "/psx/Tony Hawks Pro Skater 2.PBP",
+    core: "psx",
+    bios: "/roms/psx/bios/SCPH1001.BIN",
+    desc: "Skate radical com trilha sonora marcante e jogabilidade viciante.",
+    image: "/roms/psx/images/Tony Hawk's Pro Skater 2-thumb.png",
+    rating: "0.90",
+    players: "1-2",
+    releasedate: "2000-06-01"
+  },
+  {
+    id: "9",
+    name: "Silent Hill",
+    path: "psx/Silent Hill.PBP",
+    core: "psx",
+    bios: "/roms/psx/bios/SCPH1001.BIN",
+    desc: "Terror psicológico com ambientação sombria e trilha sonora perturbadora.",
+    image: "/roms/psx/images/Silent Hill-thumb.png",
+    rating: "0.89",
+    players: "1",
+    releasedate: "1999-06-01"
+  },
+  {
+    id: "10",
+    name: "Spyro: Year of the Dragon",
+    path: "psx/Spyro the Dragon.PBP",
+    core: "psx",
+    bios: "/roms/psx/bios/SCPH1001.BIN",
+    desc: "Exploração 3D com personagens carismáticos e fases criativas.",
+    image: "/roms/psx/images/Spyro 3 Year Of The Dragon-thumb.png",
+    rating: "0.88",
+    players: "1",
+    releasedate: "2000-06-01"
+  }
+];
