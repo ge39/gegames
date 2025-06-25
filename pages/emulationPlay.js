74ad86e9ddfd2d1bad1d499bbe749eb1b4915a1e
unknown <ge39.medeiros@gmail.com>
2025-06-25 19:23:33 -0300

    change bios schp5501.bin

--- pages/emulationPlay.js
+++ pages/emulationPlay.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import styles from '../styles/Emulation.module.css';
 import Navbar from '../components/Navbar';
-import PeerConnection from "../components/PeerConnection";
+// import PeerConnection from "../components/PeerConnection";
 
 export default function Emulation() {
   const { query } = useRouter();
