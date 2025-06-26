835e3d18f9536eeb98d3afa5e5f869cdb6508281
unknown <ge39.medeiros@gmail.com>
2025-06-26 20:11:59 -0300

    remove roms ps1

--- pages/emulationPlay.js
+++ pages/emulationPlay.js
@@ -6,7 +6,7 @@ export default function EmulationPlay() {
       <h1>Jogando com EmulatorJS</h1>
       <Emulator
         biosUrl="/roms/psone/files/scph5501.bin"
-        gameUrl="/roms/psone/Bust-A-Move 4.PBP"
+        gameUrl="/roms/psone/Chessmaster 3D.PBP"
         core="beetle-psx"
       />
     </div>
