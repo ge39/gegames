7de666df5a3ed50a76faca3dae2227fc10a51c8e
unknown <ge39.medeiros@gmail.com>
2025-06-26 20:07:31 -0300

    remove roms ps1

--- components/Emulator.js
+++ components/Emulator.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default function Emulator({ gameUrl = '', biosUrl = '', core = 'beetle-psx' }) {
   useEffect(() => {
-    if (!gameUrl || !core) {
+    if (!gameUrl || !core || !biosUrl) {
       console.error("Parâmetros faltando: gameUrl ou core");
       return;
     }
