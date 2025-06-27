8497a587b96b62aa071ff124653506f27b82bf46
ge39 <ge39.medeiros@gmail.com>
2025-06-27 06:47:47 -0300

    Update emulationPlay.js

--- pages/emulationPlay.js
+++ pages/emulationPlay.js
@@ -5,9 +5,20 @@ export default function EmulationPlay() {
     <div>
       <h1>Jogando com EmulatorJS</h1>
       <Emulator
-        biosUrl="/roms/psone/files/scph5501.bin"
-        gameUrl="/roms/psone/Bust-A-Move 4.PBP"
-        core="beetle-psx"
+        // biosUrl="/roms/psone/files/scph5501.bin"
+        // gameUrl="/roms/psone/Bust-A-Move 4.PBP"
+        // core="beetle-psx"
+           <script>
+            EJS_player = "#game";
+            EJS_core = "psx";
+            EJS_color = "#0064ff";
+            EJS_startOnLoaded = true;
+            EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+            EJS_gameUrl = "/roms/psone/Bust-A-Move 4.PBP";
+            EJS_biosUrl = "/roms/psone/files/scph5501.bin";
+        </script>
+        <script src="https://cdn.emulatorjs.org/stable/data/loader.js"></script>
+
       />
     </div>
   );
