5276dcfc6fb1847631af402ffa43109be6d028ff
ge39 <ge39.medeiros@gmail.com>
2025-06-27 07:07:39 -0300

    Update emulationPlay.js

--- pages/emulationPlay.js
+++ pages/emulationPlay.js
@@ -1,25 +1,36 @@
-import Emulator from '../components/Emulator';
+// import Emulator from '../components/Emulator';
+
+import { useEffect } from 'react';
 
 export default function EmulationPlay() {
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // Define as variáveis globais do EmulatorJS
+      window.EJS_player = "#game";
+      window.EJS_core = "psx";
+      window.EJS_color = "#0064ff";
+      window.EJS_startOnLoaded = true;
+      window.EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
+      window.EJS_gameUrl = "/roms/psone/Chessmaster 3D.PBP";
+      window.EJS_biosUrl = "/roms/psone/files/scph5501.bin";
+
+      // Carrega o loader.js dinamicamente
+      const script = document.createElement('script');
+      script.src = "https://cdn.emulatorjs.org/stable/data/loader.js";
+      script.async = true;
+      document.body.appendChild(script);
+
+      return () => {
+        // Limpeza se necessário
+        document.body.removeChild(script);
+      };
+    }
+  }, []);
+
   return (
     <div>
       <h1>Jogando com EmulatorJS</h1>
-      <Emulator
-        // biosUrl="/roms/psone/files/scph5501.bin"
-        // gameUrl="/roms/psone/Bust-A-Move 4.PBP"
-        // core="beetle-psx"
-           <script>
-            EJS_player = "#game";
-            EJS_core = "psx";
-            EJS_color = "#0064ff";
-            EJS_startOnLoaded = true;
-            EJS_pathtodata = "https://cdn.emulatorjs.org/stable/data/";
-            EJS_gameUrl = "/roms/psone/Chessmaster 3D.PBP";
-            EJS_biosUrl = "/roms/psone/files/scph5501.bin";
-        </script>
-        <script src="https://cdn.emulatorjs.org/stable/data/loader.js"></script>
-
-      />
+      <div id="game" style={{ width: "640px", height: "480px", maxWidth: "100%" }}></div>
     </div>
   );
 }
