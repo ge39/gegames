7fbe2f6c12b0fa9ee5f4c123e76c42e355357f5f
ge39 <ge39.medeiros@gmail.com>
2025-06-28 20:13:39 -0300

    Update emulationPlay.js

--- pages/emulationPlay.js
+++ pages/emulationPlay.js
@@ -1,13 +1,21 @@
 import Emulator from '../components/Emulator';
 
 export default function EmulationPlay() {
+  const bios = "/roms/psone/files/scph1001.bin";
+  const game = "/roms/psone/Ace Combat 2.PBP";
+  const core = "psx";
+
   return (
     <div>
       <h1>Jogando com EmulatorJS</h1>
+
+      <p><strong>Core:</strong> {core}</p>
+      <p><strong>BIOS:</strong> {bios.split('/').pop()}</p>
+
       <Emulator
-        biosUrl="/roms/psone/files/scph5501.bin"
-        gameUrl="/roms/psone/Crash Bash.PBP"
-        core="beetle-psx"
+        biosUrl={bios}
+        gameUrl={game}
+        core={core}
       />
     </div>
   );
