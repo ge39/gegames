1fe0fbaa36e6a5941a18867455d68890a26266a0
unknown <ge39.medeiros@gmail.com>
2025-07-25 22:05:32 -0300

    update style jogos-online

--- components/GameCard.js
+++ components/GameCard.js
@@ -5,7 +5,7 @@ export default function GameCard({ game }) {
   return (
     <div className={styles.card}>
       {/* <img src={game.image} alt={game.alt} className={styles.image} /> */}
-
+      <div style={{fontSize:'12px',fontWeight:'bold',padding:'10px',color:'#0000e6'}}>{game.name}</div>
       {game.image ? (
             <Image
               src={game.image}
@@ -17,9 +17,9 @@ export default function GameCard({ game }) {
           ) : (
             <p className={styles.noImage}>Imagem não disponível</p>
           )}
-         <div style={{fontSize:'12px',fontWeight:'bold',marginTop:'10px'}}>{game.name}</div>
+         
          {/* <small style={{fontSize:'10px'}}>{game.desc}</small> */}
-         <div style={{fontSize:'10px'}}>Players: {game.players} | Rating: {game.rating}</div>
+         <div style={{fontSize:'10px',padding:'10px',color:'#0000e6'}}>Players: {game.players} | Rating: {game.rating}</div>
         <a   href={`/emulation?jogo=${encodeURIComponent(game.path)}&core=${encodeURIComponent(game.core)}`}
             className={styles.playNow}
           >
