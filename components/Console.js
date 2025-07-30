a2ee8aeafc4630e34eb7ddd5dc24d7dd81fbc87f
ge39 <ge39.medeiros@gmail.com>
2025-07-30 08:20:04 -0300

    Update Console.js

--- components/Console.js
+++ components/Console.js
@@ -9,7 +9,7 @@ export default function Console() {
         <Link style={{ textDecoration: "none" }} href="/gamelistGba">Gba - </Link>
         <Link style={{ textDecoration: 'none' }} href="/gamelistMegadrive">Megadrive - </Link>
         <Link style={{ textDecoration: 'none' }} href="/gamelistSnes">Super Nintendo - </Link>
-        <Link style={{ textDecoration: 'none' }} href="/adult-games">X X X</Link> 
+        <Link style={{ textDecoration: 'none' }} href="/">X X X</Link> 
       </h2>
     </div>
   );
