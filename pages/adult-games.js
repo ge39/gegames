c851260e13378f81cfc022d33501d671b2bb62b5
ge39 <ge39.medeiros@gmail.com>
2025-07-30 14:49:00 -0300

    Update adult-games.js

--- pages/adult-games.js
+++ pages/adult-games.js
@@ -1,9 +1,19 @@
 
 // pages/adult-games.tsx
 import AdultGamesSection from "../components/AdultGamesSection";
+import SEOHead from "@/components/SEOHead";
 
 export default function AdultGamesPage() {
   return (
+    <>
+        <SEOHead
+        title="Jogos Adultos Arcade Online | GeGames 18+"
+        description="Explore jogos retrô adultos em estilo arcade. Títulos raros e nostálgicos para maiores de 18 anos, jogáveis diretamente no navegador."
+        keywords="jogos adultos, arcade adulto, retrô 18+, gegames adultos, jogos proibidos, fliperama 18+, nostálgicos"
+        image="https://gegames.vercel.app/images/capa-adult.png"
+        url="https://gegames.vercel.app/gamelistArcadeAdult"
+      />
+    </>
     <main className="min-h-screen bg-gray-100 py-10">
       <AdultGamesSection />
     </main>
