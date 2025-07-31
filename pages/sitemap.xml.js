c96e73982bf365c4c729d1480a490980347413c2
ge39 <ge39.medeiros@gmail.com>
2025-07-31 08:46:16 -0300

    Update sitemap.xml.js

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,4 +1,6 @@
 // pages/sitemap.xml.js
+import { arcadeGames } from '../data/arcadeGames'; // ajuste para importar de onde preferir
+
 const SITE_URL = 'https://gegames.vercel.app';
 
 export async function getServerSideProps({ res }) {
@@ -12,42 +14,37 @@ export async function getServerSideProps({ res }) {
     '/como-jogar'
   ];
 
-  // Exemplo de rotas dinâmicas de jogos
-  const dynamicGames = [
-    'super-mario-world',
-    'street-fighter-ii',
-    'sonic-the-hedgehog',
-    'mortal-kombat'
-  ];
+  const dynamicGames = arcadeGames.map(game => game.slug); // Ajuste aqui se precisar
 
   const allUrls = [
     ...staticPages.map((page) => `
       <url>
         <loc>${SITE_URL}${page}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
         <priority>0.8</priority>
       </url>`),
     ...dynamicGames.map((slug) => `
       <url>
         <loc>${SITE_URL}/games/${slug}</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
         <priority>0.7</priority>
       </url>`)
   ];
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${allUrls.join('')}
+  ${allUrls.join('\n')}
   </urlset>`;
 
   res.setHeader('Content-Type', 'text/xml');
   res.write(sitemap);
   res.end();
 
-  return {
-    props: {}
-  };
+  return { props: {} };
 }
 
 export default function Sitemap() {
   return null;
 }
-
