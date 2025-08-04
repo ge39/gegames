72ced345abe76ee43ad9f15b1be0ef939f50a221
ge39 <ge39.medeiros@gmail.com>
2025-08-04 07:52:10 -0300

    Update route.js

--- sitemap.xml/route.js
+++ sitemap.xml/route.js
@@ -1,1 +1,39 @@
+// app/sitemap.xml/route.js
+
+export async function GET() {
+  const baseUrl = "https://gegames.vercel.app";
+
+  const staticPages = [
+    "",
+    "/gamelistArcade",
+    "/gamelistSnes",
+    "/gamelistAtari",
+    "/gamelistMegadrive",
+    "/como-jogar",
+  ];
+
+  const urls = staticPages
+    .map(
+      (path) => `
+    <url>
+      <loc>${baseUrl}${path}</loc>
+      <changefreq>weekly</changefreq>
+      <priority>0.8</priority>
+    </url>`
+    )
+    .join("");
+
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+>
+  ${urls}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
 
