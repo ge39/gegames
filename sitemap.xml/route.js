5666d206285e4af14003286a53e0296d34664cec
ge39 <ge39.medeiros@gmail.com>
2025-08-04 11:36:57 -0300

    Update route.js

--- sitemap.xml/route.js
+++ sitemap.xml/route.js
@@ -1,66 +1,243 @@
-import { arcadeGames } from "@/data/arcadeGames";
-// Importe os outros arrays de jogos da mesma forma, se existirem:
- import { snesGames } from "@/data/snesGames";
- import { atariGames } from "@/data/atariGames";
- import { gbaGames } from "@/data/gbaGames";
- import { megadriveGames } from "@/data/MegadriveGames";
-
-export async function GET() {
-  const baseUrl = "https://gegames.vercel.app";
-
-  const staticPages = [
-    "",
-    "/gamelistArcade",
-    "/gamelistSnes",
-    "/gamelistAtari",
-    "/gamelistGba",
-    "/gamelistMegadrive",
-    "/como-jogar",
-  ];
-
-  // URLs estáticas
-  const staticUrls = staticPages
-    .map(
-      (path) => `
-    <url>
-      <loc>${baseUrl}${path}</loc>
-      <changefreq>weekly</changefreq>
-      <priority>0.8</priority>
-    </url>`
-    )
-    .join("");
-
-  // Todos os jogos juntos (adicione os arrays de jogos conforme seus arquivos)
-  const allGames = [
-    ...arcadeGames,
-     ...snesGames,
-     ...atariGames,
-     ...gbaGames,
-     ...MegadriveGames,
-  ];
-
-  // URLs dinâmicas dos jogos, filtrando para garantir que id exista
-  const gameUrls = allGames
-    .filter((game) => game?.id) // evita undefined
-    .map(
-      (game) => `
-    <url>
-      <loc>${baseUrl}/games/${game.id}</loc>
-      <changefreq>monthly</changefreq>
-      <priority>0.7</priority>
-    </url>`
-    )
-    .join("");
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+This XML file does not appear to have any style information associated with it. The document tree is shown below.
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  ${staticUrls}
-  ${gameUrls}
-</urlset>`;
-
-  return new Response(xml, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
-}
+<url>
+<loc>https://gegames.vercel.app</loc>
+<lastmod>2025-08-04T11:09:33.499Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/gamelistArcade</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/gamelistAtari</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/gamelistGba</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/gamelistMegadrive</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/adult-games</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/como-jogar</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.8</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+<url>
+<loc>https://gegames.vercel.app/games/undefined</loc>
+<lastmod>2025-08-04T11:09:33.509Z</lastmod>
+<changefreq>monthly</changefreq>
+<priority>0.7</priority>
+</url>
+</urlset>
