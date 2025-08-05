b2fe018b325d8a4a1e44bc6e524748fc4d871ebc
ge39 <ge39.medeiros@gmail.com>
2025-08-05 11:51:46 -0300

    Update GameCard.js

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
+import StarsRating from '@/components/StarsRating';
 import styles from '../styles/GameCard.module.css'; // ou seu arquivo CSS
 
 const GameCard = ({ game }) => {
