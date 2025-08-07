6b1c0969699979f29b8230e7fdbf26332ff7e5b0
ge39 <ge39.medeiros@gmail.com>
2025-08-07 15:08:22 -0300

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import styles from '../styles/Footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; {new Date().getFullYear()}Fliperama Retrô. Feito com ❤️ para os amantes de jogos clássicos.{' '}
+      <p>&copy; {new Date().getFullYear()}Fliperama Retrô. Feito com ❤️ para os amantes de jogos clássicos.{' '}</p>
       <p><a href="#">Política de Privacidade</a></p>
     </footer>
   );
